4e6c9e42484df4c708d8b86b519b0d059a94f8fe
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-09-05 02:54:18 -0400

    Finished Syllable counter and Partial word counter

---
+++ C++/test/FleschGrade.cpp
@@ -0,0 +1,11 @@
+/*Program to analize a text and determine its difficulty using the Flesch Grading index.
+ * Author: Jarrett Melnick
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+	return 0;
+}
